da70db5300cb140366b9a1d799dc07779fd2349d
Gabriel <gabriel.mgran20@hotmail.com>
2022-12-13 14:30:52 -0300

    update
    Ajustes no html, adicionei a pagina Js e alguns comandos.

---
+++ imc.js
@@ -0,0 +1,25 @@
+const calcular = document.getElementById(`calcular`);
+
+
+function imc () {
+
+   const res = document.querySelector(`#res`)
+
+   let nome = document.getElementById(`nome`).value;
+   let altura = document.getElementById(`altura`).value;
+   let peso = document.getElementById(`peso`).value;
+
+
+   if(nome !== "") {
+    alert (`preenchido`)
+   }
+
+
+
+   }
+
+
+
+
+
+calcular.addEventListener(`click`, imc)
